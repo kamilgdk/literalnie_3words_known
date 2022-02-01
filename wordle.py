925b4a38fa89d0191bc07c7a0d8e35611568fcbb
kamil_gdk <95642854+kamilgdk@users.noreply.github.com>
2022-02-01 20:32:23 +0100

    commenting stuff

--- wordle.py
+++ wordle.py
@@ -1,4 +1,4 @@
-# script to get all valid 5-letter polish words where only 3 consecutive letters are known
+# script to get all valid polish words where only few consecutive letters are known
 # the output is validated by sjp.pl dictionary
 # position of letters can be adjusted in line 10 (x_AAA_y by default), can be changed to AAA_x_y or x_y_AAA
 
@@ -16,11 +16,11 @@ def find_possible_words(input_word):
 def pl_dict_check(word):
     input = find_possible_words(word)
 
-    with open("/Users/kamil/Desktop/Python1stproject/slowa.txt", "r") as f:
+    with open("slowa.txt", "r") as f:  #dictionary is 40mb file, you can download it from here: https://sjp.pl/slownik/growy/ and enter the local path 
         lines = [line.rstrip('\n') for line in f]
 
     for i in input:
         if i in lines:
             print(i + ' in the dict')
 
-print(pl_dict_check("ila"))
+print(pl_dict_check("ila")) # insert the 3-character string (for literalnie.fun only) or any other string of choice
